d1e477a0ccf248e985123635ebdb12beebf4a726
liujishuai <jishuailiu@gmail.com>
2018-01-31 20:26:33 +0800

    邮件接收工具

--- src/main/java/com/jeyson/tools/Utils.java
+++ src/main/java/com/jeyson/tools/Utils.java
@@ -1,10 +1,8 @@
 package com.jeyson.tools;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.jeyson.tools.mail.ReadMailUtils;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 
 /**
@@ -13,17 +11,10 @@ import org.slf4j.LoggerFactory;
  * Description:
  */
 public class Utils {
-    //Log logger = LogFactory.getLog(getClass());
-    Logger logger= LoggerFactory.getLogger(getClass());
-    Log log=LogFactory.getLog(getClass());
 
     @Test
-    public void testLogger() {
-        String sk1="SKKK";
-        String sk2="sss";
-        String sk3="ssss";
-        String sk4="11111";
-        logger.info("Hello:{},{},{},{}",sk1,sk2,sk3,sk4);
+    public void testLogger() throws Exception {
+        ReadMailUtils.resceive();
 
     }
 }
